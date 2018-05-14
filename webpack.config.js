0baee9bd148fc5d3f7f8aaac805729fa7d8aaad7
Vladimir Rodkin <mail@vovanr.com>
2018-05-14 14:36:56 +0300

    Add `babel-loader`

--- webpack.config.js
+++ webpack.config.js
@@ -5,14 +5,30 @@ module.exports = {
     entry: {
         app: ['./src/app.js']
     },
-    
+
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: '[name].js'
     },
 
+    target: "web",
+
     module: {
         rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['env']
+                        }
+                    },
+                ],
+            },
+
+            // Must be after `babel-loader`
             {
                 test: /\.exec\.js$/,
                 use: ['script-loader']
